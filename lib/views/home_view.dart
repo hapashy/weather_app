200ee75a5a061725a8f2f320f361a5e26ea81c13
Abdelrhman Habashy <45797969+hapashy@users.noreply.github.com>
2024-06-05 12:08:19 +0300

    ui done

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/views/search_view.dart';
 import 'package:weather_app/widget/no_weather_body.dart';
 import 'package:weather_app/widget/weather_info_body.dart';
 
@@ -7,12 +8,29 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-       appBar: AppBar(
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+  onPressed: () {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const SearchView()), 
+    );
+  },
+  icon: const Icon(
+    Icons.search,
+    color: Colors.white,
+  )
+)
+        ],
         backgroundColor: Colors.blue,
-        title: const Text('Weather App',style: TextStyle(color: Colors.white),),
+        title: const Text(
+          'Weather App',
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-    body: const WeatherInfoBody(),
+      body: const WeatherInfoBody(),
     );
   }
 }
---
+++ lib/views/search_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class SearchView extends StatelessWidget {
+  const SearchView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(
+          color: Colors.white,
+        ),
+        backgroundColor: const Color.fromARGB(255, 238, 160, 43),
+        title: const Text(
+          'Search a City',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                  suffixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    
+                    borderSide: BorderSide(
+                      color: Color.fromARGB(255, 238, 160, 43),
+                    ),
+                    
+                    
+                  ),
+                  labelText: 'Search',
+                  hintText: 'Enter City Name'),
+                  
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
