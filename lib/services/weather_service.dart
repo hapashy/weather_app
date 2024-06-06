2ee360715f353d82f9f63e7fe028d67825a4f198
Abdelrhman Habashy <45797969+hapashy@users.noreply.github.com>
2024-06-06 14:23:45 +0300

    create service api

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,4 +1,21 @@
-class WeatherService 
-{
+import 'package:dio/dio.dart';
+import 'package:weather_app/models/weather_model.dart';
 
+class WeatherService {
+  final Dio dio;
+
+  final String baseUrl = 'https://api.weatherapi.com/v1';
+  final String apiKey = 'f4db0b244a1f4c96b9d72124240606';
+  WeatherService(this.dio);
+  Future<WeatherModel?> getCurrentWeather({required String cityName}) async {
+    try {
+      Response response = await dio
+          .get('$baseUrl/forecast.json?key=$apiKey&q=$cityName&days=1');
+
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+      return weatherModel;
+    } catch (e) {
+      return null;
+    }
+  }
 }
