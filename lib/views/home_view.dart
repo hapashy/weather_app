6bfe815e94dc0b11ee0c199455f58b6e2f93154f
Abdelrhman Habashy <45797969+hapashy@users.noreply.github.com>
2024-06-12 12:31:56 +0300

    integrate cubit

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather_app/cubits/get_weather_cubit/get_weather_states.dart';
 import 'package:weather_app/views/search_view.dart';
 import 'package:weather_app/widget/no_weather_body.dart';
 import 'package:weather_app/widget/weather_info_body.dart';
@@ -34,7 +37,16 @@ class _HomeViewState extends State<HomeView> {
           style: TextStyle(color: Colors.white),
         ),
       ),
-      body: const NoWeatherBody(),
+      body:
+          BlocBuilder<GetWeatherCubit, WeatherState>(builder: (context, state) {
+        if (state is WeatherInitialState) {
+          return const NoWeatherBody();
+        } else if (state is WeatherLoadedState) {
+          return const WeatherInfoBody();
+        } else {
+          return const Text('opps there was an error');
+        }
+      }),
     );
   }
 }
